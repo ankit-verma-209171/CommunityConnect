3b4d97862631a6bb6e850c97a06c3be304bb3cd1
Ankit Verma <ankitverma209171@gmail.com>
2023-04-14 10:40:48 +0530

    phone text field having country code added

--- app/src/main/java/com/codeitsolo/communityconnect/auth/ui/LoginScreen.kt
+++ app/src/main/java/com/codeitsolo/communityconnect/auth/ui/LoginScreen.kt
@@ -1,55 +1,66 @@
 package com.codeitsolo.communityconnect.auth.ui
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.codeitsolo.communityconnect.utils.PhoneTextField
+import com.codeitsolo.communityconnect.utils.getErrorStatus
+import com.codeitsolo.communityconnect.utils.getFullPhoneNumber
+import com.codeitsolo.communityconnect.utils.getOnlyPhoneNumber
+import com.codeitsolo.communityconnect.utils.isPhoneNumber
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(onGetOTPClicked: (String) -> Unit = { /* Do something */}) {
-    var phoneNumber by rememberSaveable { mutableStateOf("") }
-    val context = LocalContext.current
-
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
+            .verticalScroll(rememberScrollState())
             .fillMaxSize(),
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TextField(
-            value = phoneNumber,
-            onValueChange = { phoneNumber = it },
-            placeholder = { Text(text = "Enter a valid phone number") },
-            label = { Text(text = "Phone Number") }
-        )
-        Button(
-            onClick = {
-                if (phoneNumber.isEmpty() || phoneNumber.isBlank()) {
-                    Toast.makeText(context, "Please enter valid phone number!", Toast.LENGTH_SHORT).show()
-                    return@Button
-                }
-                onGetOTPClicked(phoneNumber)
-            },
-            modifier = Modifier.padding(top = 16.dp)
-        ) {
-            Text(text = "GET OTP")
+
+        val phoneNumber = rememberSaveable { mutableStateOf("") }
+        val fullPhoneNumber = rememberSaveable { mutableStateOf("") }
+        val onlyPhoneNumber = rememberSaveable { mutableStateOf("") }
+
+        PhoneTextField(text = phoneNumber.value, onValueChange = {phoneNumber.value = it})
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(onClick = {
+            if (!isPhoneNumber()) {
+                fullPhoneNumber.value = getFullPhoneNumber()
+                onlyPhoneNumber.value = getOnlyPhoneNumber()
+                onGetOTPClicked(fullPhoneNumber.value)
+            } else {
+                fullPhoneNumber.value = "Error"
+                onlyPhoneNumber.value = "Error"
+            }
+        }) {
+            Text(text = "Login")
         }
+
+        Text(
+            text = "Full Phone Number: ${fullPhoneNumber.value}",
+            color = if (getErrorStatus()) Color.Red else Color.Green
+        )
+
+        Text(
+            text = "Only Phone Number: ${onlyPhoneNumber.value}",
+            color = if (getErrorStatus()) Color.Red else Color.Green
+        )
     }
 }
 
